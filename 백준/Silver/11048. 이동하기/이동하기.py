6a8f84cb1bb6e6cf80d50f8e7a48bcd18e360b07
MINGYU PARK <156400439+miiiingyuuu@users.noreply.github.com>
2025-07-14 16:39:59 +0900

    [Silver II] Title: 이동하기, Time: 440 ms, Memory: 80572 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -10,33 +10,16 @@ input = sys.stdin.readline
 
 
 def solve():
-    dp = board
+    dp = [[0] * (m+1) for _ in range(n+1)]
 
-    for r in range(n):
-        for c in range(m):
-            # board[0][0]은 그냥 그 자리 출발이니까 그 값을 그대로 유지하기 위해 조건 걸기
-            if r == 0 and c == 0:
-                continue
+    for r in range(1, n+1):
+        for c in range(1, m+1):
+            dp[r][c] = max(dp[r-1][c], dp[r][c-1], dp[r-1][c-1]) + board[r-1][c-1]
 
-            # 매 칸을 조사할때 이전의 왼쪽, 왼쪽 대각 위, 위의 값을 더한 값을 비교해서 최대값을 갱신
-            max_val = 0
-            for d in range(3):
-                nr = r - dr[d]
-                nc = c - dc[d]
-                if 0 <= nr < n and 0 <= nc < m:
-                    if board[r][c] + dp[nr][nc] > max_val:
-                        max_val = board[r][c] + dp[nr][nc]
-
-            dp[r][c] = max_val
-
-    return dp[n-1][m-1]
+    return dp[n][m]
 
 
 n, m = map(int, input().split())
-board = [list(map(int ,input().split())) for _ in range(n)]
-
-# 갈 수 있는 방향: 아래, 오른쪽, 대각 오른 아래
-dr = [1, 0, 1]
-dc = [0, 1, 1]
+board = [list(map(int, input().split())) for _ in range(n)]
 
 print(solve())
