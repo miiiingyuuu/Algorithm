4017eb3009040b4163d7d552068adc7c8c8e98d7
MINGYU PARK <156400439+miiiingyuuu@users.noreply.github.com>
2025-08-09 12:02:08 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,14 @@
+function solution(numbers) {
+    const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let answer = 0
+    
+    for (let i = 0; i < 10; i++) {
+        if (numbers.includes(i)) {
+            continue
+        }   else {
+            answer += i
+        }
+    }
+    
+    return answer
+}
