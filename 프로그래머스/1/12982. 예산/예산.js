2014ccd54916b846e24e2e66a18b3b9688dc76dc
MINGYU PARK <156400439+miiiingyuuu@users.noreply.github.com>
2025-08-21 16:50:43 +0900

    [level 1] Title: 예산, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,18 @@
+function solution(d, budget) {
+    d.sort((a, b) => a - b);
+    
+    let answer = 0;
+    let sum = 0;
+    
+    for (let i = 0; i < d.length; i++) {
+        sum += d[i];
+        
+        if (sum > budget) {
+            break;
+        }
+        
+        answer++;
+    }
+    
+    return answer;
+}
