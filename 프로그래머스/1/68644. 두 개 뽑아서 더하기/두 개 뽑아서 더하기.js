5c4a0b6433ecec3bf31d60f8f68d009a962abe19
MINGYU PARK <156400439+miiiingyuuu@users.noreply.github.com>
2025-08-28 16:30:16 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.93 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,16 @@
+function solution(numbers) {
+    const answer = [];
+    
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            const sum = numbers[i] + numbers[j];
+            
+            if (!answer.includes(sum)) {
+                answer.push(sum);
+            }
+        }
+        
+    }
+    
+    return answer.sort((a, b) => a - b);    
+}
