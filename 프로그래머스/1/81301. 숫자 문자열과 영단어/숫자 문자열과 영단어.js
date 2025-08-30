b865e63c317085bb99725f8207ebccbf07edbf4d
MINGYU PARK <156400439+miiiingyuuu@users.noreply.github.com>
2025-08-30 20:13:17 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    const words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+    words.forEach((word, index) => {
+        s = s.split(word).join(index);
+    });
+    
+    return Number(s);
+}
